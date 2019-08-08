a6f1dc049ae1f12c24b51b7b91f0c6ebf2875e90
Elizaveta Mironovich <elizavetamironovich@yandex.ru>
2019-08-08 14:54:47 +0300

    Fixed indecies of X in quadruple_constraints()

--- quadruple_constraint.m
+++ quadruple_constraint.m
@@ -1,14 +1,14 @@
 function F_row = quadruple_constraint(i, j, k, x, y, X, R)
     F_row = zeros(1, 3, 6);
     %fc:
-        F_row(1, 1, :) = (y(i) - y(k)) * mult_R(R, 1, X(j) - X(i)) ...
-                        -(x(i) - x(j)) * mult_R(R, 2, X(k) - X(i));
+        F_row(1, 1, :) = (y(i) - y(k)) * mult_R(R, 1, X(:, j) - X(:, i)) ...
+                        -(x(i) - x(j)) * mult_R(R, 2, X(:, k) - X(:, i));
     %fs:
-        F_row(1, 2, :) = -(y(i) - y(k)) * mult_R(R, 2, X(j) - X(i)) ...
-                         -(x(i) - x(j)) * mult_R(R, 1, X(k) - X(i));
+        F_row(1, 2, :) = -(y(i) - y(k)) * mult_R(R, 2, X(:, j) - X(:, i)) ...
+                         -(x(i) - x(j)) * mult_R(R, 1, X(:, k) - X(:, i));
     %1:
-        F_row(1, 2, :) = -(y(i) - y(k)) * x(j) * mult_R(R, 3, X(j) - X(i)) ...
-                         +(x(i) - x(j)) * y(k) * mult_R(R, 3, X(k) - X(i));
+        F_row(1, 2, :) = -(y(i) - y(k)) * x(j) * mult_R(R, 3, X(:, j) - X(:, i)) ...
+                         +(x(i) - x(j)) * y(k) * mult_R(R, 3, X(:, k) - X(:, i));
 end
 
 function sum = mult_R(R, i, X)
