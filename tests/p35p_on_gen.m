673dddc8c624da6996c2314dafdd966dbf5ce899
Elizaveta Mironovich <elizavetamironovich@yandex.ru>
2019-08-19 20:56:14 +0300

    Added a check for zero solutions and now this script finds a vector of all relative differences of the focal distances

--- tests/p35p_on_gen.m
+++ tests/p35p_on_gen.m
@@ -1,24 +1,46 @@
 kol = 1000;
-max_diff = -1;
+%max_diff = -1;
+sum = 0;
+zero_solutions = 0;
+
+f_diffs = zeros(1, kol);
+% times = zeros(1, kol);
 
 for ind = 1 : kol
     [X, x, y, R_gen, C_gen, f_gen, P] = generate_data();
     e = 1e-6;
     [solution_num, f_sol, R_sol, T_sol] = p35p_solver(X, x, y, e);
+%     p35p_handle = @() p35p_solver(X, x, y, e);
+%     times(ind) = timeit(p35p_handle);
     min_diff = 10000000000000000;
     for i = 1 : solution_num
         f = f_sol(i);
         T = T_sol(:, i);
         R = R_sol(:, 3*solution_num - 2 : 3*solution_num);
         C = (-diag([f, f, 1])*R)\T;
-        diff = norm([abs(f - f_gen), max(abs(C - C_gen)), max(max(abs(R - R_gen)))]);
+        diff = abs(f - f_gen);
         if diff < min_diff
             min_diff = diff;
         end
     end
-    if min_diff > max_diff
-        max_diff = min_diff;
+    
+    
+%     if min_diff > e
+%         disp(min_diff);
+%         disp(X); disp([x, y]); disp(R_gen); disp(C_gen); disp(f_gen); disp(P);
+%     end
+    if solution_num ~= 0
+        f_diffs(ind - zero_solutions) = diff / f_gen;
+    else
+        zero_solutions = zero_solutions + 1;
     end
+%     if min_diff > max_diff  && solution_num ~= 0
+%         max_diff = min_diff;
+%     end
 end
 
-disp(max_diff);
+% disp(max_diff);
+% disp(sum / kol);
+% disp(zero_solutions);
+
+% histogram(log10(f_diffs(1:end-zero_solutions)));
