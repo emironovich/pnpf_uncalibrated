7a1571de9f7a5fa1306fb476a600f214d14dec46
Elizaveta Mironovich <elizavetamironovich@yandex.ru>
2019-08-26 22:30:11 +0300

    Add first draft of the ww34-35 report with density plots.
    - Add four plots with standard deviation from 0 to1.5. Ones with non-zero stdev look very different from the one with zero stdev which is troubling.
    - Change matlab sript so it that makes data for the report in csv fromat.
    - Add preliminary results.

--- tests/p35p_on_gen_with_noise.m
+++ tests/p35p_on_gen_with_noise.m
@@ -1,20 +1,24 @@
 N = 10000;
 
-stats_median = zeros(4, 11);
+%stats_median = zeros(4, 11);
+stats = zeros(11, N);
+zero_solutions = zeros(11, 1);
 
 for d = 0:0.5:5
-    stats = zeros(3, N);
     valid = false(N, 1);
 
     p35p = @(X,x,y,e)p35p_solver(X,x,y,e);
     %uncomment for MEX-based solver
     p35p = @(X,x,y,e)p35p_solver_mex(X,x,y,e);
+    %uncomment for MEX-based solver in double-precision
+    %do not forget to change the name of the output file
+    %p35p = @(X,x,y,e)p35p_solver_double_mex(X,x,y,e);
     eps = 1e-6;
     % uncomment for single-precision evaluation
     eps = 1e-1;
 
     tic
-    parfor ind = 1:N
+    for ind = 1:N
         [X, x, y, R_gen, C_gen, f_gen, P] = generate_data(d);
         start = tic;
         [solution_num, f_sol, R_sol, T_sol] = p35p(X, x, y, eps);
@@ -45,26 +49,25 @@ for d = 0:0.5:5
         end
 
         if solution_num ~= 0
-            stats(:, ind) = [min_diff / f_gen; diff_R; diff_C];
-            valid(ind) = true;    
+            stats(2*d+1, ind) = min_diff / f_gen;
+            valid(ind) = true;
+        else 
+            stats(2*d+1, ind) = -1;
         end
+      
     end
     toc
 
-    zero_solutions = numel(find(~valid));
-
-    stats = stats(:, valid);
-    stats_median(:, 2*d + 1) = [median(stats, 2); zero_solutions/N]; 
+    zero_solutions(2*d+1) = numel(find(~valid));
+    %stats_median(:, 2*d s+ 1) = [median(stats, 2); zero_solutions/N]; 
 end
 
-ff = figure('Name', 'Focal diff to deviation');
-p = plot(0:0.5:5, stats_median(1, :), 'Marker', 'o', 'Color', 'r');
+filename = 'noisy_stats_single_mex.csv';
+%uncomment for MEX-based solver in double-precision
+%filename = 'noisy_stats_double_mex.csv';
 
-fr = figure('Name', 'Rotation diff to deviation');
-plot(0:0.5:5, stats_median(2, :), 'Marker', 'o', 'Color', 'm');
+% f=fopen(filename, 'wt');
+% fprintf(f, 'stdev0.0,stdev0.5,stdev1.0,stdev1.5,stdev2.0,stdev2.5,stdev3.0,stdev3.5,stdev4.0,stdev4.5,stdev5.0,N\n');
+% fclose(f);
 
-fc = figure('Name', 'Center diff to deviation');
-plot(0:0.5:5, stats_median(3, :), 'Marker', 'o', 'Color', 'b');
-
-fz = figure('Name', 'Zero solution ration to deviation');
-plot(0:0.5:5, stats_median(4, :), 'Marker', 'o', 'Color', 'k');
+dlmwrite(filename, stats');
