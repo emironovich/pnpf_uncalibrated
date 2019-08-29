33649898b94f84dd79d33647791b25b540872e89
Elizaveta Mironovich <elizavetamironovich@yandex.ru>
2019-08-29 14:42:15 +0300

    Fix solve_3Q3() finction and add tests with extra output
    Fix some minor issues like wrong signs and indexing. Add auxiliary output for testing and tests themselves.

--- 3Q3/find_yz.m
+++ 3Q3/find_yz.m
@@ -3,7 +3,7 @@ function [y, z] = find_yz(M, x)
     mons = [x^4, x^3, x^2, x, 1];
     for i = 1 : 3
         for j = 1 : 3
-            M_subs(i, j) = M(:, i, j) * mons;
+            M_subs(i, j) = mons * M(:, i, j);
         end
     end
     [Q, ~] = qr(M_subs');
--- 3Q3/solve_3Q3.m
+++ 3Q3/solve_3Q3.m
@@ -1,18 +1,19 @@
-function [n, xs, ys, zs] = solve_3Q3(c, e) %c -- 3x10 coefficients matrix
+function [n, xs, ys, zs, A, P, P_prime, M, pol] = solve_3Q3(c, e) %c -- 3x10 coefficients matrix
 %SOLVE_3Q3 Summary of this function goes here
 %   Detailed explanation goes here
     A = find_A(c);
     P = find_P(c);
+    P_prime = zeros(3, 3, 3);
     for i = 1 : 3
-        P(:, :, i) = A\P(:, :, i);
+        P_prime(:, :, i) = A\P(:, :, i);
     end
-    M = find_M(P);
+    M = find_M(P_prime);
     pol = find_det_M(M);
     xs_complex = roots(pol');
     xs = zeros(1, length(xs_complex));
     n = 0;
     for i = 1 : length(xs_complex)
-        if imag(xs_complex) < e
+        if abs(imag(xs_complex(i))) < e
             n = n + 1;
             xs(n) = xs_complex(i);
         end
@@ -34,16 +35,16 @@ end
 function P = find_P(c)
     P = zeros(3, 3, 3); %[x^2, x, 1]
     for i = 1 : 3
-        P(i, 1, :) = [0, c(i, 4), c(i, 8)];        %y
-        P(i, 2, :) = [0, c(i, 5), c(i, 9)];        %z
-        P(i, 3, :) = [c(i, 1), c(i, 7), c(i, 10)]; %1
+        P(i, 1, :) = [0, -c(i, 4), -c(i, 8)];        %y
+        P(i, 2, :) = [0, -c(i, 5), -c(i, 9)];        %z
+        P(i, 3, :) = [-c(i, 1), -c(i, 7), -c(i, 10)]; %1
     end
 end
 
 function d = find_det_M(M)
     d = conv(M(:, 1, 1), find_det2(M(:, 2:3, 2:3))) - ...
-        conv(M(:, 1, 2), find_det2(cat(3, M(:, 1:2, 1), M(:, 1:2, 3)))) + ...
-        conv(M(:, 1, 3), find_det2(M(:, 1:2, 1:2)));
+        conv(M(:, 1, 2), find_det2(cat(3, M(:, 2:3, 1), M(:, 2:3, 3)))) + ...
+        conv(M(:, 1, 3), find_det2(M(:, 2:3, 1:2)));
 end
 
 function d = find_det2(M)
