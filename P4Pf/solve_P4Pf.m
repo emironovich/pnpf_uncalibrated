34e783ad3882da82276c0c74e22e7b1294cdd126
Elizaveta Mironovich <elizavetamironovich@yandex.ru>
2019-09-06 15:12:31 +0300

    Add a naive recovering of rotation and translation.
    The function solve_P4Pf() now returns pairs of rotation R and translation T multiplied by 1/det(R)^(1/3)

--- P4Pf/solve_P4Pf.m
+++ P4Pf/solve_P4Pf.m
@@ -1,6 +1,8 @@
 function [n, fs, Rs, Ts, A, N, D, eqs] = solve_P4Pf(X, u, v, e)
 %SOLVE_3Q3 Summary of this function goes here
 %       X = [p1, p2, p3, p4], pi = [4, 1]; X(:, i) <-> (u(i), v(i))
+%       if f is a correct foal length, then [R, T] = [R, T] / sign(d)*abs(d)^(1/3);
+%       where d = det(R)
     X = [X; ones(1, 4)];
     A = find_A(X, u, v);
     [Q, ~] = qr(A');
@@ -9,16 +11,18 @@ function [n, fs, Rs, Ts, A, N, D, eqs] = solve_P4Pf(X, u, v, e)
     eqs = find_eqs([N; D]); 
     [n, xs, ys, zs] = solve_3Q3(eqs(1:3, :), e); %we may choes another 3 out of 4 eq-s
     fs = zeros(1, n);
+    Rs = zeros(3, 3*n);
+    Ts = zeros(3, n);
     for i = 1 : n
         P = [N; D]*[xs(i); ys(i); zs(i); 1];
         P = [P(1:4)'; P(5:8)'; P(9:12)'];
         w = sqrt(sum(P(3, 1:3).^2)/sum(P(1, 1:3).^2));
         fs(i) = 1/w;
+        RT = P;
+        RT(3, :) = RT(3, :) / w;
+        Rs(:, 3*i - 2: 3*i) = RT(:, 1:3);
+        Ts(:, i) = RT(:, 4);
     end
-    Rs = [];
-    Ts = [];
-    %next we need to find w = 1/f
-    %and then we can find R and T
 end
 
 function A = find_A(X, u, v)
--- P4Pf/tests/diff_to_cvs.m
+++ P4Pf/tests/diff_to_cvs.m
@@ -4,14 +4,29 @@ stats = zeros(N, 1);
 
 for ind = 1 : N
     [X, u, v, R, C, f, P] = generate_data();
-    [num, fs] = solve_P4Pf(X, u, v, e);
+    T = -R*C;
+    [num, fs, Rs, Ts] = solve_P4Pf(X, u, v, e);
     min_diff = inf;
+    min_T = zeros(3, 1);
+    min_R = zeros(3);
+    min_f = 0;
     for i = 1 : num
         if abs(fs(i) - f) < min_diff
             min_diff = abs(fs(i) - f);
+            min_f = fs(i);
+            min_T = Ts(:, i);
+            min_R = Rs(:, 3*i - 2: 3*i);
         end
     end
     stats(ind) = min_diff/f;
+    d = det(min_R);
+    mult = sign(d)*abs(d)^(1/3);
+    min_R = min_R / mult;
+    min_T = min_T / mult;
+%     R
+%     min_R
+%     T
+%     min_T
 end
 
 filename = 'P4Pf_focus_stats.csv';
