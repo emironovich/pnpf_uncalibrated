7707c20c0db3d8fcc8acdcc469ee06d1494917cc
Elizaveta Mironovich <elizavetamironovich@yandex.ru>
2022-05-12 22:31:23 +0300

    Remove reports

--- reports/m08/write_to_csv.m
+++
@@ -1,7 +0,0 @@
-function write_to_csv(filename, arr, headers)
-    f=fopen(filename, 'wt');
-%     fprintf(f, 'dF,dR,dC,dt,N\n');
-    fprintf(f, '%s\n', headers);
-    fclose(f);
-    dlmwrite(filename, arr, '-append');
-end
